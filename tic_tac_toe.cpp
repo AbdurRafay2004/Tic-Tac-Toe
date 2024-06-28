db79949519500a98c78146607d9c213b6e47ee93
Tahsin2004 <rafay.mail2004@gmail.com>
2024-06-28 22:42:35 +0600

    board

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -2,94 +2,120 @@
 #include <string>
 #include <ctime>
 #include <windows.h>
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
 // Function to change text color
-void textcolor(int color) {
+void textcolor(int color)
+{
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(h, color);
     // RED = 4, GREEN = 2, BLUE = 1, YELLOW = 6, WHITE = 15
 }
-
-string PlayerInput(char tbl[][3], string *, string *);
-class player
+class Game
 {
-private:
-    string name = "Computer"; // player name computer by default for AI player
-    int totalmoves;           // moves counter
-
 public:
-    player(); // constructor
+    Game() : totalMoves(0){ fill_board(); }
+    void fill_board();
+protected:
+    static const int BSIZE = 3;         // Board size
+    int maxMoves = 9;      // Maximum number of moves
+    int minMovesToWin = 5; // Minimum number of moves to win
+    char board[BSIZE][BSIZE];           // Game board
+    int totalMoves;                     // Counter for total moves
+
 };
-player::player()
+// Function to initialize the board
+void Game::fill_board()
 {
-    totalmoves = 0;
+    for (int row = 0; row < BSIZE; row++)
+    {
+        for (int col = 0; col < BSIZE; col++)
+        {
+            board[row][col] = '-';
+        }
+    }
 }
 
-char menu(){
-    char choice;
-    
-    
-	textcolor(1);   cout << "\t\t\t[1] "; textcolor(6);  cout << "Player vs Player" << endl;
-	textcolor(1);   cout << "\t\t\t[2] "; textcolor(6);  cout << "Player vs Computer" << endl;
-	textcolor(1);   cout << "\t\t\t[0] "; textcolor(6);  cout << "Quit Game" << endl;
-    textcolor(15);
-	cin >> choice;
-	return choice;
-}
-void projectMembers()
+class TicTacToe : public Game
 {
+};
+
+class TicTacToeVsAI : public TicTacToe
+{
+};
+
+char menu()
+{
+    char choice;
     textcolor(4);
     cout << "\t\t\t\t      ***  TIC - TAC - TOE  ***\n\n";
-	cout << "\t\t\t\tWELCOME TO TIC-TAC-TOE BY TEAM ROCKET\n" << endl;
-    textcolor(3); //change color to green
+    cout << "\t\t\t\tWELCOME TO TIC-TAC-TOE BY TEAM ROCKET\n"
+         << endl;
+    textcolor(3); // change color to green
     std::cout
-    << "\t\t\t                     :: Abdur Rafay      (1013)\n"
-    << "\t\t\tContributions        :: Al Abedin Wasee  (1013)\n"
-    << "\t\t\t                     :: Younus Ali Molla (1013)\n"
-    << "\t\t\t                     \n";
-    
-    textcolor(15); //reset color
+        << "\t\t\t                     :: Abdur Rafay      (1013)\n"
+        << "\t\t\tContributions        :: Al Abedin Wasee  (1013)\n"
+        << "\t\t\t                     :: Younus Ali Molla (1013)\n"
+        << "\t\t\t                     \n";
 
-    //shows menu
-    char command;
-	do {
-		command = menu();
-		switch (command) {
-		case '1':
-        //player vs player
-        
-			break;
-		case '2':
-		//player vs AI
-			break;
-		}
-		system("pause");
-	} while (command != '0');
-	exit(0);
+    textcolor(15); // reset color
+
+    textcolor(1);
+    cout << "\t\t\t[1] ";
+    textcolor(6);
+    cout << "Player vs Player" << endl;
+    textcolor(1);
+    cout << "\t\t\t[2] ";
+    textcolor(6);
+    cout << "Player vs Computer" << endl;
+    textcolor(1);
+    cout << "\t\t\t[0] ";
+    textcolor(6);
+    cout << "Quit Game" << endl;
+    textcolor(15);
+    cin >> choice;
+    return choice;
 }
-void command() {
-	
+
+void command()
+{
+    // shows menu
+    char command;
+    do
+    {
+        command = menu();
+        switch (command)
+        {
+        case '1':
+            // player vs player
+
+            break;
+        case '2':
+            // player vs AI
+            break;
+        }
+        system("pause");
+    } while (command != '0');
+    exit(0);
 }
 
 int main()
 {
-    projectMembers();//shows intro informations
-    char board[3][3]{
-       {'-', '-', '-'},
-       {'-', '-', '-'},
-       {'-', '-', '-'}
-    };
+    command(); // shows intro informations
+    // char board[3][3]{
+    //     {'-', '-', '-'},
+    //     {'-', '-', '-'},
+    //     {'-', '-', '-'}};
 
-    string Plyr1;
-    string Plyr2;
-    cout << "Enter player 1 name : ";
-    getline(cin, Plyr1);
-    cout << "Enter player 2 name : ";
-    getline(cin, Plyr2);
-
-
-
+    // string Plyr1;
+    // string Plyr2;
+    // cout << "Enter player 1 name : ";
+    // getline(cin, Plyr1);
+    // cout << "Enter player 2 name : ";
+    // getline(cin, Plyr2);
 
     return 0;
 }
