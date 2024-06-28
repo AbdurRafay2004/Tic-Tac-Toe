ef89852564204b7ccf537396fd47600acf82d6e2
Tahsin2004 <rafay.mail2004@gmail.com>
2024-06-28 23:16:32 +0600

    board design 😶

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -14,18 +14,26 @@ void textcolor(int color)
     SetConsoleTextAttribute(h, color);
     // RED = 4, GREEN = 2, BLUE = 1, YELLOW = 6, WHITE = 15
 }
+
+// Base class representing the general structure of a game
 class Game
 {
 public:
-    Game() : totalMoves(0){ fill_board(); }
+    Game() : totalMoves(0) { fill_board(); }
     void fill_board();
+    virtual void displayBoard() const = 0;            // Pure virtual function for displaying the board
+    //need to get user input
+    //checkwin
+    //checkdraw
+    //switch player
+    virtual void playGame() = 0;                      // Function to play the game
+    //ai
 protected:
-    static const int BSIZE = 3;         // Board size
-    int maxMoves = 9;      // Maximum number of moves
-    int minMovesToWin = 5; // Minimum number of moves to win
-    char board[BSIZE][BSIZE];           // Game board
-    int totalMoves;                     // Counter for total moves
-
+    static const int BSIZE = 3; // Board size
+    int maxMoves = 9;           // Maximum number of moves
+    int minMovesToWin = 5;      // Minimum number of moves to win
+    char board[BSIZE][BSIZE];   // Game board
+    int totalMoves;             // Counter for total moves
 };
 // Function to initialize the board
 void Game::fill_board()
@@ -41,8 +49,55 @@ void Game::fill_board()
 
 class TicTacToe : public Game
 {
+public:
+    // Constructor to initialize the board
+    TicTacToe()
+    {
+        totalMoves = 0;
+        fill_board();
+    }
+    void displayBoard() const override;            // Override function to display the board
+    void playGame() override;           // Override function to
 };
 
+void TicTacToe::displayBoard() const
+{
+    /*
+    cout << " --------------\n";
+    for (int row = 0; row < BSIZE; row++)
+    {
+        cout << " | ";
+        for (int col = 0; col < BSIZE; col++)
+        {
+            cout << board[row][col] << "  |";
+        }
+        cout << "\n --------------\n";
+    }
+    */
+    /*
+    cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2];
+        cout << "\t\t 1 | 2 | 3 " << endl;
+        cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2];
+        cout << "\t\t 4 | 5 | 6 " << endl;
+        cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2];
+        cout << "\t\t 7 | 8 | 9 " << endl
+             << endl;
+    */
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            cout << " " << board[row][col];
+            if (col < 2) cout << " |";
+        }
+    cout << "\t\t " << (row * 3) + 1 << " | " << (row * 3) + 2 << " | " << (row * 3) + 3;
+    if (row < 2) cout << endl << "-----------\t\t-----------";
+    cout << endl;
+    }
+    cout << endl;
+}
+
+void TicTacToe::playGame(){
+    displayBoard();
+}
 class TicTacToeVsAI : public TicTacToe
 {
 };
@@ -84,6 +139,7 @@ void command()
 {
     // shows menu
     char command;
+    Game *game = nullptr;
     do
     {
         command = menu();
@@ -91,6 +147,8 @@ void command()
         {
         case '1':
             // player vs player
+            game = new TicTacToe();
+            game->playGame();
 
             break;
         case '2':
